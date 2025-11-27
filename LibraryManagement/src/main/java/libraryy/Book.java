8048148bf8ac57ced9bb96bba8be41ff305db042
hp <hp@DESKTOP-D86UIDA>
2025-11-27 12:49:44 -0800

    new class

--- LibraryManagement/src/main/java/libraryy/Book.java
+++ LibraryManagement/src/main/java/libraryy/Book.java
@@ -1,5 +1,27 @@
+
 package libraryy;
-
+import java.util.*;
 public class Book {
+	private String title;
+    private String author;
+    private String isbn;
+    private boolean available = true;
 
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getIsbn() { return isbn; }
+    public boolean isAvailable() { return available; }
+    public void setAvailable(boolean available) { this.available = available; }
+
+    @Override
+    public String toString() {
+        return String.format("Book[Title=%s, Author=%s, ISBN=%s, Available=%s]",
+                title, author, isbn, available ? "Yes" : "No");
+    }
 }
